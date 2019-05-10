ec417399ffdc35daa996d03aea38f4d5c2cc67a9
sakibahmad24 <sakibahmad24@gmail.com>
2019-05-10 15:19:22 +0600

    arrays in ruby hash in ruby

---
+++ arrays.rb
@@ -0,0 +1,8 @@
+friends = ["sadman", "tanvir", "rabid"]
+
+puts friends.length()
+puts friends.reverse()
+puts friends.sort()
+
+puts friends[0]
+puts friends[0, 2]
---
+++ hash.rb
@@ -0,0 +1,16 @@
+divisions = {
+  "Dhaka" => "DHK",
+  "Sylhet" => "SYL",
+  "Chattogram" => "CTG"
+}
+
+puts divisions
+puts divisions["Dhaka"]
+
+dist = {
+  1 => "DHK",
+  2 => "SYL",
+  3 => "CTG"
+}
+
+puts dist[1]
