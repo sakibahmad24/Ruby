3f9fcf1b52ef6e5b6ea550e9135b915ce10e54d9
sakibahmad24 <sakibahmad24@gmail.com>
2019-05-10 06:46:01 +0600

    working with string string manipulation triangle printing

--- start.rb
+++ start.rb
@@ -1,1 +1,17 @@
-print "Hello world"
+# print "Kazi sakib ahmad"
+# puts"this is a put"
+
+my_name = "adfasd sakib adfa  afasf    "
+my_age = "24"
+
+puts my_name.upcase()
+puts my_name.strip()
+puts my_name.length()
+puts my_age.length()
+
+puts my_name.include? "sab" #should return false
+
+puts my_name[0] #accesing 0'th element in string
+puts my_name[0, 4] #accesing 0-3'rd element in string
+
+puts my_name.index("s")
