f95139d3713c0a6227be17faa654424002e2a1ac
sakibahmad24 <sakibahmad24@gmail.com>
2019-05-13 02:13:23 +0600

    for loops

---
+++ for_loop.rb
@@ -0,0 +1,14 @@
+
+divisions = ["Dhaka", "chittagong", "Sylhet", "Rajshahi", "Khulna", "Barishal", "Rangpur"]
+
+for division in divisions
+  puts division #printing all the divisions name using for loop
+end
+
+for index in 0..100
+  puts index #printing 0 to 5
+end
+
+6.times do |index|
+  puts index
+end
